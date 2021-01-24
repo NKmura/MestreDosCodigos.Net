fb763412e1c8a6c23acf56e097dac541f350bf81
Rafael Nakamura de Souza <rafael.souza@db1.com.br>
2021-01-24 18:32:41 -0300

    Implementados exercícios 5 e 6

--- MestreDosCodigos/NivelEscudeiro/ExerciciosConsole/Console5.cs
+++ MestreDosCodigos/NivelEscudeiro/ExerciciosConsole/Console5.cs
@@ -4,12 +4,46 @@ using System.ComponentModel;
 namespace NivelEscudeiro.ExerciciosConsole
 {
     [Description("Console - Exercício 5")]
-    public class Console5
+    public class Console5: IExecutar
     {
         public void Executar()
         {
             ImprimirDescricaoExercicio();
-            throw new NotImplementedException();
+            int a = LerInt("Valor de a:");
+            int b = LerInt("Valor de b:");
+            int c = LerInt("Valor de c:");
+
+            //Passo 1 - Achar o valor de Delta
+            var delta = (Math.Pow(b, 2) - (4 * a * c));
+
+            var raizDelta = Math.Sqrt(delta);
+            if (Double.IsNaN(raizDelta))
+            {
+                Console.WriteLine("A equação de 2º grau não possúi raizes reais!");
+                return;
+            }
+
+            //passo 2 - Bhaskara
+            var x1 = (-b + raizDelta)/ (2 * a);
+
+            var x2 = (-b - raizDelta) / (2 * a);
+
+            Console.WriteLine(@"
+Resultados");
+            Console.WriteLine();
+            Console.WriteLine($"Valor x': {x1}");
+            Console.WriteLine($"Valor x'': {x2}");
+        }
+
+        public int LerInt(string descricao)
+        {
+            int valor=0;
+            while (valor == 0)
+            {
+                Console.Write(descricao);
+                Int32.TryParse(Console.ReadLine(), out valor);
+            }
+            return valor;
         }
 
         private void ImprimirDescricaoExercicio()
--- MestreDosCodigos/NivelEscudeiro/ExerciciosConsole/Console6.cs
+++ MestreDosCodigos/NivelEscudeiro/ExerciciosConsole/Console6.cs
@@ -7,12 +7,45 @@ using System.Text;
 namespace NivelEscudeiro.ExerciciosConsole
 {
     [Description("Console - Exercício 6")]
-    public class Console6
+    public class Console6: IExecutar
     {
         public void Executar()
         {
             ImprimirDescricaoExercicio();
-            throw new NotImplementedException();
+
+            int parametroRef = 0; // variável precisa ser inicializada
+            int parametroOut;     // inicialização opcional
+
+            Console.WriteLine($"Variável Ref antes da chamada: {parametroRef}");
+            Console.WriteLine($"Variável Out antes da chamada: Não inicializada");
+
+            MetodoParametroRef(ref parametroRef);
+            MetodoParametrouOut(out parametroOut);
+
+
+            Console.WriteLine($"Variável Ref após chamada: {parametroRef}");
+            Console.WriteLine($"Variável Out após chamada: {parametroOut}");
+            
+        }
+
+        public void MetodoParametroRef(ref int variavel)
+        {
+            Console.WriteLine(@"
+-A palavra chave ref faz com que um argumento seja passado como referencia, isso significa que quando o argumento sofre 
+atribuição dentro do método ela se refletirá no método de chamada. 
+Um argumento REF deverá obrigatóriamente ser inicializado no método de chamada antes de ser passado no método chamado.
+");
+            variavel = 10;
+        }
+
+        public void MetodoParametrouOut(out int variavel)
+        {
+            Console.WriteLine(@"
+-A palavra-chave out faz com que uma variavel seja passada por referencia (ref) porém diferente de ref a variavel não 
+precisa ser iniciada antesde ser passada, outra diferença é que um parametro out precisa obrigatóriamente receber alguma 
+atribuição dentro do método antes do mesmo ser retornado.
+");
+            variavel = 20;
         }
 
         private void ImprimirDescricaoExercicio()
